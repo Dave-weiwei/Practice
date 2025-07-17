68f01676eed15d769d26f7fb07e847c4bb233ce0
Dave <ok858585ok@gmail.com>
2025-07-17 16:48:32 +0800

    調整flask ip狀況

--- web/app.py
+++ web/app.py
@@ -63,4 +63,4 @@ def login():
         return jsonify({"error": "帳號或密碼錯誤"}), 401
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
