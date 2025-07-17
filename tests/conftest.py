eeec7d097d5f6d219beb8a1d3752499051842bff
Dave <ok858585ok@gmail.com>
2025-07-17 16:24:28 +0800

    webdriver修改對應CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,8 @@ import allure
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 
@@ -22,9 +24,22 @@ def driver(request):
     if browser == "chrome":
         service = ChromeService(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service)
+        chrome_options = ChromeOptions()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-gpu")  # 雖然在 headless 模式 GPU 不重要，但仍建議加
+        chrome_options.add_argument("--disable-infobars")
+        chrome_options.add_argument("--window-size=1920,1080")
+        
+        
     elif browser == "firefox":
         service = FirefoxService(GeckoDriverManager().install())
         driver = webdriver.Firefox(service=service)
+        firefox_options = FirefoxOptions()
+        firefox_options.add_argument("--headless")
+        service = FirefoxService(GeckoDriverManager().install())
+        driver = webdriver.Firefox(service=service, options=firefox_options)
     else:
         raise ValueError(f"❌ 不支援的瀏覽器: {browser}")
 
