c86e0cc53a3f55f950be6fd2bea2589989f6b49d
Dave <ok858585ok@gmail.com>
2025-08-14 18:08:07 +0800

    更新docker設定

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,16 +26,18 @@ def driver(request):
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--disable-gpu")  # 雖然在 headless 模式 GPU 不重要，但仍建議加
+        chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--disable-infobars")
         chrome_options.add_argument("--window-size=1920,1080")
-        service = ChromeService(ChromeDriverManager().install())
+        service = ChromeService("/usr/local/bin/chromedriver")
         driver = webdriver.Chrome(service=service, options=chrome_options)
+
     elif browser == "firefox":
         firefox_options = FirefoxOptions()
         firefox_options.add_argument("--headless")
-        service = FirefoxService(GeckoDriverManager().install())
+        service = FirefoxService("/usr/bin/geckodriver")
         driver = webdriver.Firefox(service=service, options=firefox_options)
+
     else:
         raise ValueError(f"❌ 不支援的瀏覽器: {browser}")
 
